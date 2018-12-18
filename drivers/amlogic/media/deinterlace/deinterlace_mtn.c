e7ddc750b964281db65e2028f86da916d66c3e13
Jihong Sui <jihong.sui@amlogic.com>
2018-12-18 21:56:38 +0800

    deinterlace: pq: adjust pulldown setting for tl1 [1/1]
    PD#SWPL-3182
    
    Problem:
    1.VLSI(Feijun) fine-tune pulldown setting for TL1, G12A/B;
    2.fine tune combing_glbmot_radprat by VLSI(yanling.liu);
    
    Solution:
    finetune setting.
    
    Verify:
    tl1
    
    Change-Id: Ie65cec8b216752600dfd54ee6be5302150282774
    Signed-off-by: Jihong Sui <jihong.sui@amlogic.com>

--- drivers/amlogic/media/deinterlace/deinterlace_mtn.c
+++ drivers/amlogic/media/deinterlace/deinterlace_mtn.c
@@ -297,7 +297,7 @@ struct combing_status_s *adpative_combing_config(unsigned int width,
 }
 void mtn_int_combing_glbmot(void)
 {
-	if (is_meson_tl1_cpu() || is_meson_tm2_cpu()) {/*from VLSI yanling.liu*/
+	if (is_meson_tl1_cpu()) {/*from VLSI yanling.liu*/
 		combing_glbmot_radprat[0] = 30;
 	}
 }
@@ -629,23 +629,10 @@ static int di_debug_readreg;
 module_param(di_debug_readreg, int, 0644);
 MODULE_PARM_DESC(di_debug_readreg, "di_debug_readreg");
 
-/*from VLSI yanling.liu, the patch fix TL1 1080I in some dark */
-/*scenes and roller coasters have small sawtooth, when turn off*/
-/*combing_fix_en, set the registers*/
-void fix_tl1_1080i_sawtooth_patch(void)
-{
-	DI_Wr(0x1741, 0x0A0A1A22);
-	DI_Wr(0x1742, 0x0a100101);
-	DI_Wr(0x1743, 0x01020420);
-	DI_Wr(0x1744, 0x32210404);
-	DI_Wr(0x17a9, 0x0a100505);
-	DI_Wr(0x17aa, 0x04040101);
-	DI_Wr(0x17ab, 0x0a0a0a0a);
-	DI_Wr(0x17ac, 0x0f100101);
-	DI_Wr(0x17ad, 0x04040606);
-	DI_Wr(0x17ae, 0x02030202);
-	DI_Wr(0x17af, 0x60020a60);
-}
+static int combing_cnt;
+int combing_diff_min = 2000;
+int combing_diff_max = 2000;
+int combing_cnt_thd = 10;
 
 int adaptive_combing_fixing(
 	struct combing_status_s *cmb_status,
@@ -701,6 +688,13 @@ int adaptive_combing_fixing(
 	glb_mot[2] = glb_mot[1];
 	glb_mot[1] = glb_mot[0];
 	glb_mot[0] = frame_diff;
+	if (glb_mot[0] < combing_diff_min)
+		combing_cnt = combing_cnt + 1;
+	else
+		combing_cnt = 0;
+	if (glb_mot[0] < combing_diff_min && glb_mot[1] > combing_diff_max &&
+		combing_cnt <= combing_cnt_thd)
+		glb_mot[0] = glb_mot[1];
 	glb_mot_avg5 =
 		(glb_mot[0] + glb_mot[1] + glb_mot[2] + glb_mot[3] +
 		 glb_mot[4]) / 5;
